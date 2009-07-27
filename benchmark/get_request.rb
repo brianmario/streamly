b381ad04a653cc13a05ccbc24655a892549bd539
Brian Lopez <seniorlopez@gmail.com>
2009-07-27 09:42:16 -0700

    changing load path again

--- benchmark/get_request.rb
+++ benchmark/get_request.rb
@@ -1,26 +1,33 @@
 # encoding: UTF-8
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
 
 require 'net/http'
-require 'lib/streamly'
+require 'streamly'
 require 'benchmark'
 
 Benchmark.bm do |x|
   puts "shell out to curl"
   x.report do
-    `curl -s --compressed www.google.com`
+    (ARGV[0] || 1).to_i.times do
+      `curl -s --compressed www.google.com`
+    end
   end
   
   puts "streamly"
   x.report do
-    Streamly.get 'www.google.com'
+    (ARGV[0] || 1).to_i.times do
+      Streamly.get 'www.google.com'
+    end
   end
   
   puts "net/http"
-  req = Net::HTTP::Get.new('www.google.com')
   x.report do
-    res = Net::HTTP.start('google.com', 80) {|http|
-      http.request(req)
-    }
-    res.body
+    (ARGV[0] || 1).to_i.times do
+      req = Net::HTTP::Get.new('www.google.com')
+      res = Net::HTTP.start('google.com', 80)  do |http|
+        http.request(req)
+      end
+      res.body
+    end
   end
 end
