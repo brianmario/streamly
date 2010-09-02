1ebd4ec83eda3e6b45b277508a2c87ab61e34751
Brian Lopez <seniorlopez@gmail.com>
2010-09-01 23:51:31 -0700

    update benchmark scripts

--- benchmark/basic_request.rb
+++ benchmark/basic_request.rb
@@ -10,23 +10,23 @@ require 'benchmark'
 
 url = ARGV[0]
 
-Benchmark.bm do |x|
-  puts "Streamly"
+Benchmark.bmbm do |x|
   x.report do
+    puts "Streamly"
     (ARGV[1] || 1).to_i.times do
       Streamly.get(url)
     end
   end
-  
-  puts "Shell out to curl"
+
   x.report do
+    puts "Shell out to curl"
     (ARGV[1] || 1).to_i.times do
       `curl -s --compressed #{url}`
     end
   end
-  
-  puts "rest-client"
+
   x.report do
+    puts "rest-client"
     (ARGV[1] || 1).to_i.times do
       RestClient.get(url, {"Accept-Encoding" => "identity, deflate, gzip"})
     end
--- benchmark/streaming_json_request.rb
+++ benchmark/streaming_json_request.rb
@@ -11,31 +11,31 @@ require 'benchmark'
 
 url = ARGV[0]
 
-Benchmark.bm do |x|
-  puts "Streamly"
+Benchmark.bmbm do |x|
   parser = Yajl::Parser.new
   parser.on_parse_complete = lambda {|obj| }
   x.report do
+    puts "Streamly"
     (ARGV[1] || 1).to_i.times do
       Streamly.get(url) do |chunk|
         parser << chunk
       end
     end
   end
-  
-  puts "Shell out to curl"
+
   parser = Yajl::Parser.new
   parser.on_parse_complete = lambda {|obj| }
   x.report do
+    puts "Shell out to curl"
     (ARGV[1] || 1).to_i.times do
       parser.parse `curl -s --compressed #{url}`
     end
   end
-  
-  puts "rest-client"
+
   parser = Yajl::Parser.new
   parser.on_parse_complete = lambda {|obj| }
   x.report do
+    puts "rest-client"
     (ARGV[1] || 1).to_i.times do
       parser.parse RestClient.get(url, {"Accept-Encoding" => "identity, deflate, gzip"})
     end
